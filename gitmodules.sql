efb73988ab3954bc5a58cb3c0ea3718250b43d4f
Michael Lagocki <lagoma1@student.op.ac.nz>
2020-02-25 08:16:06 +1300

    Add Course Materials

---
+++ gitmodules.sql
@@ -0,0 +1,6 @@
+[submodule "oosd-course-material"]
+	path = oosd-course-material
+	url = https://github.com/otago-polytechnic-bit-courses/oosd-course-material.git
+[submodule "oosd-practicals-kammorne"]
+	path = oosd-practicals-kammorne
+	url = https://github.com/otago-polytechnic-bit-courses/oosd-practicals-kammorne.git
